3d006e48fc428c619f8200673496842d29a225ba
Abrar Muhammad Aufa <81955948+abrarmuhammadaufa@users.noreply.github.com>
2025-11-03 15:15:40 +0700

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://192.168.1.8:5000/api",
+    baseURL: "http://{$ip_address}:{$port_backend}/api",
     timeout: 15000,
 });
 
